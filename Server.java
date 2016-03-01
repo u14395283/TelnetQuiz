cd766eb7f2eade446451491d62e193b4118ee4b4
Unarine Rambani <u14004489@tuks.co.za>
2016-03-01 11:43:17 +0200

    New Server.java
    you know it

---
+++ Server.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.*;
+public class Server 
+{
+	private static ServerSocket servSock = null;
+	private static BufferedReader input = null;
+	private static BufferedWriter output = null;
+	
+	public static void main(String[] args)
+	{
+		try 
+		{
+			servSock = new ServerSocket(Integer.parseInt(args[0]));
+			System.out.println("Server is now running...");
+		} 
+		catch (NumberFormatException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		catch (IOException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		boolean quit = false;
+		
+		while(quit == false)
+		{
+			try
+			{
+				Socket client = servSock.accept();
+				
+				input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+				System.out.println("Client connected...");
+			}
+			catch(Exception e)
+			{
+				System.out.println("Failed to create socket");
+			}
+			
+		}
+	}
+}
